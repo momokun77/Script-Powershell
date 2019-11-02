02e0fe3cf7ac2119e44962e998a61102af29cce2
momokun77 <57261899+momokun77@users.noreply.github.com>
2019-11-02 12:36:10 +0100

    Update purge.ps1

--- purge.ps1
+++ purge.ps1
@@ -1,3 +1,8 @@
 ﻿param([string]$fichier, [int]$date)
 
 Get-ChildItem ($fichier) -File | Where CreationTime -lt (Get-Date).AddDays(-$date) | Remove-Item -Force
+
+#La commande Param([type]$variable), cette commande permet d'identifier la partie contenant les paramètres, et d'envoyer au script certaines données
+#Get-ChildItem ($fichier) -File Affiche le contenu du dossier entré en paramètre après le script
+# Where CreationTime -lt (Get-Date).AddDays(-$date) Cherche un fichier plus vieux de X jours, entré en paramètre secondaire après le script
+#Remove-Item -Force Supprime le fichier concerné
