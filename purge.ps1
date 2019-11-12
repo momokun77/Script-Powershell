ca89ed4de77fa40d83909551ca15d69598ba14a3
momokun77 <57261899+momokun77@users.noreply.github.com>
2019-11-12 09:14:24 +0100

    Update purge.ps1

--- purge.ps1
+++ purge.ps1
@@ -2,6 +2,9 @@
 
 Get-ChildItem ($fichier) -File | Where CreationTime -lt (Get-Date).AddDays(-$date) | Remove-Item -Force
 
+#PS C:\Users\[name_user]\desktop> .\purge.ps1 "C:\Users\[name_user]\Desktop\Nouveau dossier" 20
+#Ce mettre dans le repertoire ou se trouve le scripte avec la commande [CD] pour l'executer.
+
 #La commande Param([type]$variable), cette commande permet d'identifier la partie contenant les paramètres, et d'envoyer au script certaines données
 #Get-ChildItem ($fichier) -File Affiche le contenu du dossier entré en paramètre après le script
 # Where CreationTime -lt (Get-Date).AddDays(-$date) Cherche un fichier plus vieux de X jours, entré en paramètre secondaire après le script
